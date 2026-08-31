84128d41b575a676f64d1ba7ad0c9bfaf67b7458
marwishe <dsafronov0608@gmail.com>
2026-08-31 13:30:11 +0700

    feat(branches): add is_active field, add class Meta for admin configuration

--- apps/branches/models.py
+++ apps/branches/models.py
@@ -5,3 +5,13 @@ class Branch(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    # добавить longtitude & latitude для карты в будущем
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Филиал'
+        verbose_name_plural = 'Филиалы'
+
+    def __str__(self):
+        return self.name
