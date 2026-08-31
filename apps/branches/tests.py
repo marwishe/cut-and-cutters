cd5a6e87101670059f39c4044ea2db79210c2e0b
marwishe <dsafronov0608@gmail.com>
2026-08-31 15:53:38 +0700

    test(apps): check master model creation and branch relationship

--- apps/branches/tests.py
+++ apps/branches/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
-
+import pytest
+from apps.branches.models import Branch
 # Create your tests here.
+@pytest.mark.django_db
+class TestBranch:
+
+    def test_str_returns_name(self):
+        branch = Branch.objects.create(
+            name='Центральный',
+            address='ул. Бривибас, 100',
+            phone='+37120000000'
+        )
+        assert str(branch) == 'Центральный'
--- apps/staff/tests.py
+++ apps/staff/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
-
+import pytest
+from apps.staff.models import Master
+from apps.branches.tests import Branch
+from django.core.files.uploadedfile import SimpleUploadedFile
 # Create your tests here.
+@pytest.mark.django_db
+class TestMaster:
+
+    def test_avatar_upload_path(self, create_branch):
+        branch = create_branch()
+        dummy_image = (
+            b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xff\xff\xff\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00'
+            b'\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b'
+        )
+
+        fake_avatar = SimpleUploadedFile(
+            name='test_avatar.gif',
+            content=dummy_image,
+            content_type='image/gif'
+        )
+
+        master = Master.objects.create(name='Ирина', branch=branch, avatar=fake_avatar)
+
+        assert master.avatar.name.startswith('masters/avatar/')
+        assert 'test_avatar' in master.avatar.name
+
+    def test_str_returns_name(self, create_branch):
+        branch = create_branch()
+        master = Master.objects.create(name='Ирина', branch=branch)
+
+        assert str(master) == 'Ирина'
