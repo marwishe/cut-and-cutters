ab2f62456d0405919489dd931dce963e237bdcf6
marwishe <dsafronov0608@gmail.com>
2026-08-31 14:26:05 +0700

    chore(branches): change max_length for address and phone

--- apps/branches/models.py
+++ apps/branches/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Branch(models.Model):
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
     is_active = models.BooleanField(default=True)
     # добавить longtitude & latitude для карты в будущем
 
