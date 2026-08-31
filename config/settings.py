3f326d7dd8965f5e3e5eee63d0a6717e1e03807f
marwishe <dsafronov0608@gmail.com>
2026-08-31 13:22:42 +0700

    feat(apps): add CustomUser model, register AUTH_USER_MODEL in settings.py

--- config/settings.py
+++ config/settings.py
@@ -67,9 +67,7 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'appps.users.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
