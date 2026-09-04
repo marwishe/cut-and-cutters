9847942d5746674ed86347cfcfb06d8d2ef76288
marwishe <dsafronov0608@gmail.com>
2026-09-04 13:31:09 +0700

    feat(services): add model of master services

--- apps/services/models.py
+++ apps/services/models.py
@@ -1,3 +1,24 @@
+from apps.staff.models import Master
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    duration_minutes = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    masters = models.ManyToManyField(
+        Master,
+        related_name='services',
+        blank=True
+    )
+
+    class Meta:
+        ordering=['name']
+        verbose_name='Услуга'
+        verbose_name_plural='Услуги'
+
+    def __str__(self):
+        return self.name
