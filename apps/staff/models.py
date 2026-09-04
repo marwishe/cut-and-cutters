d3c3e9f83ae34fe5ac0376f20555bb1d91fddf19
marwishe <dsafronov0608@gmail.com>
2026-09-04 13:33:25 +0700

    feat(staff): add working hours class with master dependency

--- apps/staff/models.py
+++ apps/staff/models.py
@@ -1,3 +1,5 @@
+from django.db.models import CASCADE
+from django.db.models import IntegerChoices
 from apps.branches.models import Branch
 from config import settings
 from django.db import models
@@ -17,3 +19,28 @@ class Master(models.Model):
 
     def __str__(self):
         return self.name
+
+class WorkingHours(models.Model):
+    class Weekday(IntegerChoices):
+        MONDAY = 0, 'Понедельник'
+        TUESDAY = 1, 'Вторник'
+        WEDNESDAY = 2, 'Среда'
+        THURSDAY = 3, 'Четверг'
+        FRIDAY = 4, 'Пятница'
+        SATURDAY = 5, 'Суббота'
+        SUNDAY = 6, 'Воскресенье'
+
+    master = models.ForeignKey(Master, on_delete=CASCADE, related_name='working_hours')
+    weekday = models.IntegerField(choices=Weekday.choices)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_day_off = models.BooleanField(default=False)
+
+    class Meta:
+        ordering=['master', 'weekday']
+        unique_together=['master', 'weekday']
+        verbose_name='Рабочие часы'
+        verbose_name_plural='Рабочие часы'
+
+    def __str__(self):
+        return f'{self.master.name} – {self.get_weekday_display()}'
